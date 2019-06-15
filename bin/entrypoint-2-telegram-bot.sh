e9317bee77a9d8e3dde55079865f61a8456e7189
Douglas Muth <dmuth@dmuth.org>
2019-06-15 16:20:37 -0400

    Added Dockerfile and support infrastructure for a Telegram bot container.

---
+++ bin/entrypoint-2-telegram-bot.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Entrypoint script to be run in the Docker container
+#
+
+
+# Errors are fatal
+set -e
+
+cd /mnt
+
+if test "$DEVEL"
+then
+	echo "# "
+	echo "# Container launched in development mode."
+	echo "# "
+	echo "# Your current directiry: $(pwd)"
+	echo "# "
+	echo "# Scripts located in: /mnt/bin/"
+	echo "# "
+	exec /bin/bash
+
+else
+	/mnt/bin/2-telegram-bot.py $@
+
+fi
+
+
