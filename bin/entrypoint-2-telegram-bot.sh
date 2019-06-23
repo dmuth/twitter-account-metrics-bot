b9763382fb27485ccf72fd19bc8e8f9adc11b03e
Douglas Muth <dmuth@dmuth.org>
2019-06-23 16:31:28 -0400

    Checking to see if environment variables are set.

--- bin/entrypoint-2-telegram-bot.sh
+++ bin/entrypoint-2-telegram-bot.sh
@@ -7,6 +7,29 @@
 # Errors are fatal
 set -e
 
+if test ! "$TELEGRAM_CHAT_ID"
+then
+	echo "! "
+	echo "! Env varaible TELEGRAM_CHAT_ID is not set!"
+	echo "! Please set it and re-run this script."
+	echo "! "
+	echo "! Instructions for getting the Chat ID are in the README"
+	echo "! "
+	exit 1
+fi
+
+if test ! "$TELEGRAM_TOKEN"
+then
+	echo "! "
+	echo "! Env varaible TELEGRAM_TOKEN is not set!"
+	echo "! Please set it and re-run this script."
+	echo "! "
+	echo "! You can get the token by talking to @BotFather on Telegram."
+	echo "! "
+	exit 1
+fi
+
+
 cd /mnt
 
 if test "$DEVEL"
